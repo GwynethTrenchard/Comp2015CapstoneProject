b2d71ff996909609edefc3793077ff1727a5d350
David MacLeod Mayes <d.macleodmayes@gmail.com>
2026-02-18 15:07:06 -0500

    added the front end program and checked if it worked visually

---
+++ frontend/Frontend.ui/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Frontend.ui
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
