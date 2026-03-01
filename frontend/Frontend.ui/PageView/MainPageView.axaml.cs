479c8d9f327e728779191a08aa00d31f2e1ce456
David MacLeod Mayes <d.macleodmayes@gmail.com>
2026-03-01 14:18:57 -0500

    Added MainPage with basic design for choosing games

---
+++ frontend/Frontend.ui/PageView/MainPageView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Frontend.ui;
+
+public partial class MainPageView : UserControl
+{
+    public MainPageView()
+    {
+        InitializeComponent();
+    }
+}
