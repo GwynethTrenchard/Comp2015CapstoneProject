a1dc4d8e71712104695165f133f4b257290a955a
Cody Dutremble-Collins <codster672@gmail.com>
2026-03-05 08:09:30 -0500

    Added most button navigation

--- frontend/Frontend.ui/PageView/LoginPageView.axaml.cs
+++ frontend/Frontend.ui/PageView/LoginPageView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace Frontend.ui.PageView;
@@ -9,6 +10,17 @@ public partial class LoginPageView : UserControl
     public LoginPageView()
     {
         InitializeComponent();
-
+    }
+    private void OnBackClicked(object? sender, RoutedEventArgs e)
+    {
+        (TopLevel.GetTopLevel(this) as MainWindow)?.NavigateToLanding();
+    }
+    private void OnLoginClicked(object? sender, RoutedEventArgs e)
+    {
+        (TopLevel.GetTopLevel(this) as MainWindow)?.NavigateToMain();
+    }
+    private void OnRegisterClicked(object? sender, RoutedEventArgs e)
+    {
+        (TopLevel.GetTopLevel(this) as MainWindow)?.NavigateToRegister();
     }
 }
