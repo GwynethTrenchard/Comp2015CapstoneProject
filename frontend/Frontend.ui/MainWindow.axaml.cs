c644b44a714f7e106a3ea57768c9b6039b9edf2e
David MacLeod Mayes <d.macleodmayes@gmail.com>
2026-03-24 15:21:45 -0400

    Made the Quest-Journal-Notes pages and fixed some textboxes

--- frontend/Frontend.ui/MainWindow.axaml.cs
+++ frontend/Frontend.ui/MainWindow.axaml.cs
@@ -9,9 +9,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        ContentArea.Content = new LandingPageView();
+        //ContentArea.Content = new LandingPageView();
 
-        //ContentArea.Content = new FeatureView(); -> David's testing code please ignore
+        ContentArea.Content = new NotesView(); //-> David's testing code please ignore
     }
 
     public void Navigate (UserControl newPage)
