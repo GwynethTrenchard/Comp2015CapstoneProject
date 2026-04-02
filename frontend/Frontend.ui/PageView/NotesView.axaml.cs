02ed15d1f0febc0a3f017e203f2c2d34116b5183
Gwyneth Trenchard <gwyneth.trenchard@gmail.com>
2026-04-02 15:48:45 -0400

    Revert "Added NotesView, QuestView, and JournalView"

--- frontend/Frontend.ui/PageView/NotesView.axaml.cs
+++ frontend/Frontend.ui/PageView/NotesView.axaml.cs
@@ -1,52 +1,13 @@
-
+using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Avalonia.VisualTree;
-using System.Collections.Generic;
-using Frontend.ui.PageView;
+using Avalonia.Markup.Xaml;
 
 namespace Frontend.ui;
 
 public partial class NotesView : UserControl
 {
-    private List<Note> notes = new();
-
     public NotesView()
     {
         InitializeComponent();
-    }
-
-    private void OnNewNoteClicked(object? sender, RoutedEventArgs e)
-    {
-        notes.Add(new Note());
-        RefreshUI();
-    }
-
-    private void OnReturnClicked(object? sender, RoutedEventArgs e)
-    {
-        var window = TopLevel.GetTopLevel(this) as MainWindow;
-        window?.Navigate(new LandingPageView());
-    }
-
-    private void RefreshUI()
-    {
-        NotesPanel.Children.Clear();
-
-        foreach (var note in notes)
-        {
-            var box = new TextBox
-            {
-                AcceptsReturn = true,
-                Height = 120,
-                Text = note.Content
-            };
-
-            box.GetObservable(TextBox.TextProperty).Subscribe(text =>
-            {
-                note.Content = text ?? "";
-            });
-
-            NotesPanel.Children.Add(box);
-        }
     }
 }
